ff344da54c56a2854ae6ce0748b957c78cf28e30
tasta005 <tasta005@umn.edu>
2019-10-23 21:05:07 -0500

    major update to shiny table structure

--- shiny/table_app/app.R
+++ shiny/table_app/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(DT)
 library(dplyr)
+library(rsconnect)
 
 
 df <- read.csv("ccgd_export.csv")
@@ -42,120 +43,147 @@ df <- read.csv("ccgd_export.csv")
 #shinyApp(ui, server)
 
 
-#ui <- fluidPage(
-#  #User dropbox
-#  selectInput("cancer", label = "Cancer", choices = unique(df$Cancer.Type))
-#  #Print table to UI
-#  ,tableOutput("table1")
-#)
-#
-#server <- function(input,output){
-#    
-#  df_subset <- reactive({
-#    a <- subset(df, df$Cancer.Type == input$cancer)
-#    return(a)
-#  })
-#
-#  output$table1 <- DT::renderDataTable(df_subset()) #Note how df_subset() was used and not df_subset
-#
-#}
-#
-#shinyApp(ui, server)
-
-
-
-
 ui <- fluidPage(
-
-  titlePanel("mtcars"),
-
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("vs",
-                  label = "vs",
-                  choices = c(0, 1),
-                  selected = NULL,
-                  multiple = TRUE),
-      selectInput("carb",
-                  label = "carb",
-                  choices = c(1, 2, 3, 4, 6, 8),
-                  selected = NULL,
-                  multiple = TRUE),
-      selectInput("gear",
-                  label = "gear",
-                  choices = c(3, 4, 5),
-                  selected = NULL,
-                  multiple = TRUE)
-    ),
-
-
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Expression values", tableOutput("mainTable")),
-        tabPanel("ID filtering", tableOutput("table"))
-      )
-    )
-  )
+  #User dropbox
+  selectInput("Cancer",
+              label = "Cancer",
+              choices = unique(df$Cancer.Type),
+              selected = NULL,
+              multiple = TRUE),
+  selectInput("Study",
+              label = "Study",
+              choices = unique(df$Study),
+              selected = NULL,
+              multiple = TRUE),
+  #Print table to UI
+  dataTableOutput("mainTable")
 )
 
-server <- function(input, output) {
+server <- function(input,output){
 
-  samples.df <- data.frame(ID = paste0("ID", as.character(round(runif(nrow(mtcars),
-                                                                      min = 0,
-                                                                      max = 100 * nrow(mtcars))))),
-                           gear = as.factor(mtcars$gear),
-                           carb = as.factor(mtcars$carb),
-                           vs = as.factor(mtcars$vs))
-
-  values.df <- cbind(paste0("Feature", 1:20),
-                     as.data.frame(matrix(runif(20 * nrow(samples.df)), nrow = 20)))
-
-  colnames(values.df) <- c("Feature", as.character(samples.df$ID))
-
-  vs.values <- reactive({
-    if (is.null(input$vs)) {
-      return(c(0, 1))
+  Cancer.values <- reactive({
+    if (is.null(input$Cancer)) {
+      return(unique(df$Cancer.Type))
     } else {
-      return(input$vs)
+      return(input$Cancer)
     }
   })
 
-  carb.values <- reactive({
-    if (is.null(input$carb)) {
-      return(c(1, 2, 3, 4, 6, 8))
+  Study.values <- reactive({
+    if (is.null(input$Study)) {
+      return(unique(df$Study))
     } else {
-      return(input$carb)
+      return(input$Study)
     }
   })
 
-  gear.values <- reactive({
-    if (is.null(input$gear)) {
-      return(c(3, 4, 5))
-    } else {
-      return(input$gear)
-    }
+  filtered.df <- reactive({
+    return(df %>% filter(Cancer.Type %in% Cancer.values(),
+                                 Study %in% Study.values()))
   })
 
-  filtered.samples.df <- reactive({
-    return(samples.df %>% filter(gear %in% gear.values(),
-                                 vs %in% vs.values(),
-                                 carb %in% carb.values()))
+  output$mainTable <- renderDataTable({
+    filtered.df()
   })
-
-  filtered.values.df <- reactive({
-    selected.samples <- c("Feature", names(values.df)[names(values.df) %in% filtered.samples.df()$ID])
-    return(values.df %>% select(selected.samples))
-  })
-
-  output$mainTable <- renderTable({
-    filtered.values.df()
-  })
-
-  output$table <- renderTable({
-    filtered.samples.df()
-  })
-
 
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+
+# https://stackoverflow.com/questions/48926395/simplify-the-subset-of-a-table-using-multiple-conditions-in-r-shiny
+
+#ui <- fluidPage(
+#
+#  titlePanel("mtcars"),
+#
+#  sidebarLayout(
+#    sidebarPanel(
+#      selectInput("vs",
+#                  label = "vs",
+#                  choices = c(0, 1),
+#                  selected = NULL,
+#                  multiple = TRUE),
+#      selectInput("carb",
+#                  label = "carb",
+#                  choices = c(1, 2, 3, 4, 6, 8),
+#                  selected = NULL,
+#                  multiple = TRUE),
+#      selectInput("gear",
+#                  label = "gear",
+#                  choices = c(3, 4, 5),
+#                  selected = NULL,
+#                  multiple = TRUE)
+#    ),
+#
+#
+#    mainPanel(
+#      tabsetPanel(
+#        tabPanel("Expression values", tableOutput("mainTable")),
+#        tabPanel("ID filtering", tableOutput("table"))
+#      )
+#    )
+#  )
+#)
+#
+#server <- function(input, output) {
+#
+#  samples.df <- data.frame(ID = paste0("ID", as.character(round(runif(nrow(mtcars),
+#                                                                      min = 0,
+#                                                                      max = 100 * nrow(mtcars))))),
+#                           gear = as.factor(mtcars$gear),
+#                           carb = as.factor(mtcars$carb),
+#                           vs = as.factor(mtcars$vs))
+#
+#  values.df <- cbind(paste0("Feature", 1:20),
+#                     as.data.frame(matrix(runif(20 * nrow(samples.df)), nrow = 20)))
+#
+#  colnames(values.df) <- c("Feature", as.character(samples.df$ID))
+#
+#  vs.values <- reactive({
+#    if (is.null(input$vs)) {
+#      return(c(0, 1))
+#    } else {
+#      return(input$vs)
+#    }
+#  })
+#
+#  carb.values <- reactive({
+#    if (is.null(input$carb)) {
+#      return(c(1, 2, 3, 4, 6, 8))
+#    } else {
+#      return(input$carb)
+#    }
+#  })
+#
+#  gear.values <- reactive({
+#    if (is.null(input$gear)) {
+#      return(c(3, 4, 5))
+#    } else {
+#      return(input$gear)
+#    }
+#  })
+#
+#  filtered.samples.df <- reactive({
+#    return(samples.df %>% filter(gear %in% gear.values(),
+#                                 vs %in% vs.values(),
+#                                 carb %in% carb.values()))
+#  })
+#
+#  filtered.values.df <- reactive({
+#    selected.samples <- c("Feature", names(values.df)[names(values.df) %in% filtered.samples.df()$ID])
+#    return(values.df %>% select(selected.samples))
+#  })
+#
+#  output$mainTable <- renderTable({
+#    filtered.values.df()
+#  })
+#
+#  output$table <- renderTable({
+#    filtered.samples.df()
+#  })
+#
+#
+#}
+#
+#shinyApp(ui = ui, server = server)
