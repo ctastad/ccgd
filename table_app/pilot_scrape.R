3c31061fcae36b4f75dc862415e71efad0a21015
tasta005 <tasta005@umn.edu>
2019-11-03 21:39:43 -0600

    reverted from test commit

--- table_app/pilot_scrape.R
+++ table_app/pilot_scrape.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 
 df <- read.csv("ccgd_export.csv")
+upload <- data.frame(df$Mouse.ID)
 
 
 homo <- read.table("../../pl/homologene.txt", sep="\t")
-colnames(homo) <- c("homologene_id", "taxonomy_id", "gene_id", "gene_name", "gi_number", "refseq")
+colnames(homo) <- c("homologene id", "taxonomy_id", "gene_id", "gene_name", "gi_number", "refseq")
 ortho <- read.table("../../pl/orthologs.txt", sep="\t", header=T)
