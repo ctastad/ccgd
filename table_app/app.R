6bbcc88f6e18b09f63d370ad62dbb1913a0f3ecc
tasta005 <tasta005@umn.edu>
2019-11-04 21:30:45 -0600

    trying to get the goddamn etl script to work

--- table_app/app.R
+++ table_app/app.R
@@ -9,49 +9,56 @@ df <- read.csv("ccgd_export.csv")
 
 ui <- fluidPage(
 
-#  sidebarLayout(
-#    sidebarPanel(
-      #User dropbox
-    fluidRow(
-      column(3,
-        selectInput("Species",
-                    label = "Species",
-                    choices = c("Mouse", "Human", "Rat", "Drosophila", "Zebrafish"),
-                    selected = NULL),
+  #  sidebarLayout(
+  #    sidebarPanel(
+  # User dropbox
+  fluidRow(
+    column(
+      3,
+      selectInput("Species",
+        label = "Species",
+        choices = c("Mouse", "Human", "Rat", "Drosophila", "Zebrafish"),
+        selected = NULL
       ),
-      column(3,
-        selectInput("Cancer",
-                    label = "Cancer",
-                    choices = sort(unique(df$Cancer.Type)),
-                    selected = NULL,
-                    multiple = TRUE),
-      ),
-      column(3,
-        selectInput("Study",
-                    label = "Study",
-                    choices = sort(unique(df$Study)),
-                    selected = NULL,
-                    multiple = TRUE),
-      ),
-      column(3,
-        textAreaInput("Genes",
-                    label = "Genes",
-                    placeholder = "GeneA,GeneB,GeneC..."),
-      )
     ),
-      #Print table to UI
-#  dataTableOutput("mainTable")
-
-      tabsetPanel(
-        tabPanel("Search", dataTableOutput("mainTable")),
-        #tabPanel("Search", textOutput("mainTable")),
-        tabPanel("Export", dataTableOutput("mainTable1"))
-      )
+    column(
+      3,
+      selectInput("Cancer",
+        label = "Cancer",
+        choices = sort(unique(df$Cancer.Type)),
+        selected = NULL,
+        multiple = TRUE
+      ),
+    ),
+    column(
+      3,
+      selectInput("Study",
+        label = "Study",
+        choices = sort(unique(df$Study)),
+        selected = NULL,
+        multiple = TRUE
+      ),
+    ),
+    column(
+      3,
+      textAreaInput("Genes",
+        label = "Genes",
+        placeholder = "GeneA,GeneB,GeneC..."
+      ),
     )
-#)
+  ),
+  # Print table to UI
+  #  dataTableOutput("mainTable")
 
-server <- function(input,output){
+  tabsetPanel(
+    tabPanel("Search", dataTableOutput("mainTable")),
+    # tabPanel("Search", textOutput("mainTable")),
+    tabPanel("Export", dataTableOutput("mainTable1"))
+  )
+)
+# )
 
+server <- function(input, output) {
   Species.values <- reactive({
     if (is.null(input$Species)) {
       return(c("Mouse", "Human", "Rat", "Drosophila", "Zebrafish"))
@@ -78,41 +85,51 @@ server <- function(input,output){
 
   Gene.values <- reactive({
     if (input$Genes == "") {
-      return(unlist(strsplit(as.character(df[,1]), " ")))
-      #return("Wac")
+      return(unlist(strsplit(as.character(df[, 1]), " ")))
+      # return("Wac")
     } else {
       return(input$Genes)
     }
   })
 
-filtered.df <- reactive({
+  filtered.df <- reactive({
     return(df %>%
-             select(contains(Species.values()), COSMIC:Studies) %>%
-             filter(Cancer.Type %in% Cancer.values(),
-                    tolower(df[,1]) %in% tolower(
-                            unlist(strsplit(gsub("[\r\n]", ",", Gene.values()), ","))),
-                    Study %in% Study.values()))
+      select(contains(Species.values()), COSMIC:Studies) %>%
+      filter(
+        Cancer.Type %in% Cancer.values(),
+        tolower(df[, 1]) %in% tolower(
+          unlist(strsplit(gsub("[\r\n]", ",", Gene.values()), ","))
+        ),
+        Study %in% Study.values()
+      ))
   })
 
-#output$mainTable <- renderText({
-#    paste("output", input$Genes)
-#})
+  # output$mainTable <- renderText({
+  #    paste("output", input$Genes)
+  # })
 
-  output$mainTable <- renderDataTable({
-    filtered.df()},
+  output$mainTable <- renderDataTable(
+    {
+      filtered.df()
+    },
     options = list(
-      dom = 'frtip'),
-    style = "bootstrap")
+      dom = "frtip"
+    ),
+    style = "bootstrap"
+  )
 
-  output$mainTable1 <- renderDataTable({
-    filtered.df()},
+  output$mainTable1 <- renderDataTable(
+    {
+      filtered.df()
+    },
     extensions = "Buttons",
     options = list(
-      dom = 'B',
+      dom = "B",
       pageLength = -1,
-      buttons = c('copy', 'csv')),
-    style = "bootstrap")
+      buttons = c("copy", "csv")
+    ),
+    style = "bootstrap"
+  )
 }
 
 shinyApp(ui, server)
-
--- table_app/pilot_scrape.R
+++ table_app/pilot_scrape.R
@@ -2,26 +2,60 @@ library(tidyverse)
 
 df <- read.csv("ccgd_export.csv")
 upload <- read.csv("ccgd_upload.csv")
-upload <- data.frame(Mouse.ID = upload$mouse_id)
+upload <- data.frame(Mouse.ID = df$Mouse.ID)
 
 homo <- read.table("../../pl/homologene.txt", sep = "\t")
+homo <- homo[c(1,3,4)]
 colnames(homo) <- c(
   "homologene id",
-  "taxonomy_id",
   "gene_id",
-  "gene_name",
-  "gi_number",
-  "refseq"
+  "gene_name"
 )
 
 ortho <- read.table("../../pl/orthologs.txt", sep = "\t", header = T)
 
-tax_ids <- c(10090, 9606, 4932, 7227, 7955, 10116)
+# ++9606 human
+# --4932 yeast
+# --7227 drosophila
+# ++7955 zebrafish
+# -+10116 rat
+# ++10090 mouse
+
+species <- c("Mouse", "Human", "Rat", "Zebrafish", "Fly", "Yeast")
+activeSpecies <- c(1:4)
+taxIds <- c(10090, 9606, 10116, 7955, 7227, 4932)
+# Mouse (1) and Human (2) are skipped in loop due to earlier processing step
+otherTaxIds <- c(3:4)
+
+#tax_ids <- c(7955, 10116)
+#
+#  inner_join(ortho, by = c("Human.ID" = "GeneID")) %>%
+#  filter(Other_tax_id %in% tax_ids) %>%
+#  select(Mouse.ID, Human.ID, Other_tax_id, Other_GeneID)
+#  spread(unique(Other_tax_id), Other_GeneID)
 
 ccgd_table <- upload %>%
   inner_join(ortho, by = c("Mouse.ID" = "Other_GeneID")) %>%
-  select(Mouse.ID, Human.ID = GeneID) %>%
-  inner_join(ortho, by = c("Human.ID" = "GeneID")) %>%
-  filter(Other_tax_id %in% tax_ids) %>%
-  select(Mouse.ID, Human.ID, Other_tax_id, Other_GeneID) %>%
-  spread(Other_tax_id, Other_GeneID)
+  select(Mouse = Mouse.ID, Human = GeneID)
+
+for(i in c(taxIds[otherTaxIds])){
+  ccgd_table <- ccgd_table %>%
+      inner_join(filter(ortho, Other_tax_id == i),
+                 by = c("Human" = "GeneID")) %>%
+      select(-c(relationship, tax_id, Other_tax_id))
+}
+
+names(ccgd_table)[1:length(ccgd_table)] <- c(species[activeSpecies])
+
+for(i in c(taxIds[otherTaxIds])){
+  ccgd_table %>%
+      inner_join(., homo, by = c(!!i = "gene_id"))
+  paste(i)
+}
+
+x <- "Mouse"
+y <- "gene_id"
+x <- enquo(x)
+ccgd_table <- ccgd_table %>%
+    inner_join(ccgd_table, homo, by = c({{x}} = "gene_id"))
+
