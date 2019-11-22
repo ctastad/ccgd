f2c695a59f9b3605793f5fc5d6a5691925231d69
tasta005 <tasta005@umn.edu>
2019-11-22 03:08:36 -0600

    updated references process to reflect change from bibtex to csv; minor copy edits to site pages

--- scripts/ccgd_upload.sh
+++ scripts/ccgd_upload.sh
@@ -5,7 +5,7 @@
 #   File:   ccgd_upload.sh
 #   Author: Christopher Tastad (tasta005)
 #   Group:  Starr Lab - University of Minnesota
-#   Date:   2019-10-13
+#   Date:   2019-10-22
 #
 #   Function:   This script combines several disparate steps in the process of
 #               uploading files to the Candidate Cancer Gene Database server.
@@ -13,7 +13,7 @@
 #               While bringing the server current and complete.
 #   Requires:   knit_site.R, site_backup.sh, table_backup.sh, key access to the
 #               CCGD server, build_table.sh, Optional (ccgd_export.csv,
-#               ccgd_refs.bib upload files)
+#               ccgd_refs.csv upload files)
 #   Executed:   locally from the CCGD project dir
 #   Options:    -b build (TRUE,FALSE) -t table upload file -r reference upload
 #               file
@@ -52,7 +52,7 @@ then
     echo "No reference source file supplied"
 else
     echo "Putting reference source file in place"
-    cp $refs $scriptDir/../refs/ccgd_refs.bib
+    cp $refs $scriptDir/../refs/ccgd_refs.csv
 fi
 
 
@@ -89,7 +89,7 @@ echo "Syncing the project directory with the server"
 if [ -z "$refs" ] || [ -z "$table" ]
 then
 rsync -ah \
-    --exclude '/refs/ccgd_refs.bib' \
+    --exclude '/refs/ccgd_refs.csv' \
     --exclude '/table_app/ccgd_export.csv' \
     ./* swadm@hst-ccgd-prd-web.oit.umn.edu:$root
 else
