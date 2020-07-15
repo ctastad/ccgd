9432aee23647739f63e0c5ae0f52d48b0ab1d2c4
Christopher Tastad <ctastad@gmail.com>
2020-07-14 19:36:26 -0500

    testing site render encoding

--- scripts/knit_site.R
+++ scripts/knit_site.R
@@ -20,7 +20,7 @@
 setwd("..")
 
 # render core webpages
-rmarkdown::render("index.Rmd")
+rmarkdown::render("index.Rmd", encoding="UTF-8")
 rmarkdown::render("search.Rmd")
 rmarkdown::render("help.Rmd")
 rmarkdown::render("references.Rmd")
